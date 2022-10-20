60b3574c53c37795480af468bde9b33f964acbf4
ddubu5by5 <116294353+ddubu5by5@users.noreply.github.com>
2022-10-20 11:42:21 -0400

    Made a face

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
     joystickbit.initJoystickBit()
 })
+basic.showLeds(`
+    . # . # .
+    . . . . .
+    # . . . #
+    . # . # .
+    . # # # .
+    `)
 basic.forever(function () {
 	
 })
